160ccf5549f4c06567d3ad77c21c9b21eab0dbed
NishuVB07 <banakarnishant@gmail.com>
2024-07-27 17:51:59 +0530

    small modifications

--- skillforage/app/page.tsx
+++ skillforage/app/page.tsx
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-import Image from "next/image";
 import Link from "next/link";
-=======
 import React from 'react';
 import EnchancedHomePage from '../components/homePage';
->>>>>>> 0f6f2e65db1a76ae85d5a2832264e7071b81b665
 
 const Pages: React.FC = () => {
   return (
