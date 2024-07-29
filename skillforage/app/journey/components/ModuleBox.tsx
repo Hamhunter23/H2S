12f91e298a9d150025c31768ab0ee5a6eae80a3e
NishuVB07 <banakarnishant@gmail.com>
2024-07-29 15:16:22 +0530

    updated journey page

--- skillforage/app/journey/components/ModuleBox.tsx
+++ skillforage/app/journey/components/ModuleBox.tsx
@@ -12,24 +12,22 @@ const Module: React.FC<ModuleProps> = ({ name, start, details }) => {
 
 
     return (
-        <div>
-            <div className="flex flex-col gap-1.5 items-center justify-center bg-blue-500 rounded-2xl w-72 h-40">
-                <p>{name}</p>
-                <button
-                    className="bg-black rounded px-4 hover:bg-blue-700"
-                    onClick={start}
-                >
-                    Start
-                </button>
-                <button
-                    className="bg-black rounded px-4 hover:bg-blue-700"
-                    onClick={details}
-                >
-                    Details
-                </button>
-            </div>
-            
+        <div className="flex flex-col gap-1.5 items-center justify-center bg-blue-500 rounded-2xl w-72 h-40 relative m-5 myModule">
+            <p>{name}</p>
+            <button
+                className="bg-black rounded px-4 hover:bg-blue-700"
+                onClick={start}
+            >
+                Start
+            </button>
+            <button
+                className="bg-black rounded px-4 hover:bg-blue-700"
+                onClick={details}
+            >
+                Details
+            </button>
         </div>
+
     )
 };
 
