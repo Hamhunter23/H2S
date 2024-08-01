19eefaa9a260c40c1ddcaf91cdbd569df170c6f2
NishuVB07 <banakarnishant@gmail.com>
2024-08-01 19:08:29 +0530

    error fixing

--- skillforage/app/api/register/route.ts
+++ skillforage/app/api/register/route.ts
@@ -1,5 +1,5 @@
-import User from '@/app/models/user';
-import connect from '@/utils/db';
+import User from '../../models/user';
+import connect from '../../../utils/db';
 import bcrypt from "bcryptjs";
 
 import {NextResponse} from 'next/server';
